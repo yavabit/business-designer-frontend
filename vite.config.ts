dc05127e864c532704d405de57739ac7fbb564f7
Артём Кравцов <a.kravtsov@globexit.ru>
2025-08-17 17:23:01 +0300

    feat: Добавлен алиас для хуков

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       '@app': path.resolve(__dirname, './src/app'),
       '@store': path.resolve(__dirname, './src/store'),
       '@type': path.resolve(__dirname, './src/shared/types'),
+      '@hooks': path.resolve(__dirname, './src/shared/hooks'),
       '@pages': path.resolve(__dirname, './src/pages'),
     }
   }
