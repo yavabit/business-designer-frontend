193ab99f0e24859e0304fb4744cfe9ceac5af94d
Артём Кравцов <a.kravtsov@globexit.ru>
2025-08-17 17:25:05 +0300

    feat: Дополнен тип IProcess

--- src/shared/types/index.d.ts
+++ src/shared/types/index.d.ts
@@ -1,30 +1,32 @@
 interface IUser {
-  id: string
-  firstname?: string
-  lastname?: string
+	id: string
+	firstname?: string
+	lastname?: string
 	name: string
-  email: string
+	email: string
 	isAuth: boolean
 }
 
 interface IProject {
-  id: string
-  name: string
+	id: string
+	name: string
 	pict_url?: string
-  creation_user_id: string
-  creation_user_name: string
+	creation_user_id: string
+	creation_user_name: string
 	created_at: Date
 	updated_at: Date
 }
 
 interface IProcess {
-  id: string
-  name: string
-  project_id: string
+	id: string
+	name: string
+	desc: script
+	project_id: string
 	project_name: string
+	scheme: string
 	pict_url?: string
-  creation_user_id: string
-  creation_user_name: string
+	creation_user_id: string
+	creation_user_name: string
 	created_at: Date
 	updated_at: Date
 }
