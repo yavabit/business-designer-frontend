d580fd75fe2b6b3b0deba33a5f29cc5ae6d39ca5
Артём Кравцов <a.kravtsov@globexit.ru>
2025-08-17 17:24:50 +0300

    feat: Добавлена страница с процессами

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { userReducer } from './user/userSlice'
+import { processReducer } from './process/processSlice'
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+		process: processReducer
   },
 })
 
 export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
+export type AppStore = typeof store
