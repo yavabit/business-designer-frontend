ef2727029f46505ac6654584fc10646da6ca7caf
Артём Кравцов <a.kravtsov@globexit.ru>
2025-08-21 21:35:20 +0300

    feat: Сортировка, форма создания процесса

--- src/app/layouts/Layout/Layout.tsx
+++ src/app/layouts/Layout/Layout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom'
 import styles from './Layout.module.scss'
 import { Flex } from 'antd'
 import { ProjectCreationModal } from '@components/ProjectCreationModal/ProjectCreationModal'
+import { ProcessCreationModal } from '@components/ProcessCreationModal/ProcessCreationModal'
 
 export const Layout: FC = () => {
     return (
@@ -13,6 +14,7 @@ export const Layout: FC = () => {
                 <Outlet />
             </main>
             <ProjectCreationModal/>
+						<ProcessCreationModal/>
         </Flex>
     )
 }
