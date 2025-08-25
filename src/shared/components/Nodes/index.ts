a667839b4ef313dfe7bb37a36da3da99a6608151
Артём Кравцов <a.kravtsov@globexit.ru>
2025-08-25 10:30:05 +0300

    refactor: Добавлен тип для списка нод, убран лог из примера

--- src/shared/components/Nodes/index.ts
+++ src/shared/components/Nodes/index.ts
@@ -1,6 +1,7 @@
 import ExampleNode from "@components/Nodes/ExampleNode";
+import type { NodeTypes } from "@xyflow/react";
 
-const nodeTypes = {
+const nodeTypes: NodeTypes = {
 	exampleNode: ExampleNode,
 };
 
