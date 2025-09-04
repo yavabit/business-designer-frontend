e87768ddee6de5bb5a78ac17031f81d2c1bb4bd6
Артём Кравцов <a.kravtsov@globexit.ru>
2025-09-04 16:57:56 +0300

    feat: Переход на страницу login при ошибке авторизации

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -7,44 +7,45 @@ import { reset, setCredentials } from "@store/user/userSlice";
 export const baseUrl = import.meta.env.VITE_API_HOST + import.meta.env.VITE_API_URL;
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: baseUrl,
-    credentials: "include",
-    prepareHeaders: (headers, { getState }) => {
-        const token = (getState() as RootState).user.token;
-        if (token) {
-            headers.set("Authorization", `Bearer ${token}`);
-        }
-        return headers;
-    },
+	baseUrl: baseUrl,
+	credentials: "include",
+	prepareHeaders: (headers, { getState }) => {
+		const token = (getState() as RootState).user.token;
+		if (token) {
+			headers.set("Authorization", `Bearer ${token}`);
+		}
+		return headers;
+	},
 });
 
 const baseQueryWithReauth: BaseQueryFn = async (args, api, extraOptions) => {
-    let res = await baseQuery(args, api, extraOptions);
-    if (res?.error) {
-        console.log("Sending refreshToken...");
-        const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
-        if (refreshResult?.data) {
-            const email = (api.getState() as RootState).user.email;
-            api.dispatch(
-                setCredentials({
-                    ...(refreshResult.data as ICredentials),
-                    email,
-                })
-            );
-            res = await baseQuery(args, api, extraOptions);
-        } else {
-            api.dispatch(reset());
-        }
-    }
-    return res;
+	let res = await baseQuery(args, api, extraOptions);
+	if (res?.error) {
+		console.log("Sending refreshToken...");
+		const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
+		if (refreshResult?.data) {
+			const email = (api.getState() as RootState).user.email;
+			api.dispatch(
+				setCredentials({
+					...(refreshResult.data as ICredentials),
+					email,
+				})
+			);
+			res = await baseQuery(args, api, extraOptions);
+		} else {
+			api.dispatch(reset());
+			location.href = "/login"
+		}
+	}
+	return res;
 };
 
 export const baseApi = createApi({
-    reducerPath: "api",
-    baseQuery: baseQueryWithReauth,
-    // baseQuery: fetchBaseQuery({
-    //     baseUrl: import.meta.env.VITE_API_HOST + import.meta.env.VITE_API_URL,
-    // }),
-    tagTypes: Object.values(apiTags),
-    endpoints: () => ({}),
+	reducerPath: "api",
+	baseQuery: baseQueryWithReauth,
+	// baseQuery: fetchBaseQuery({
+	//     baseUrl: import.meta.env.VITE_API_HOST + import.meta.env.VITE_API_URL,
+	// }),
+	tagTypes: Object.values(apiTags),
+	endpoints: () => ({}),
 });
