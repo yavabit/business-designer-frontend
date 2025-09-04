806700dbda07c6714bd5868cde8d5c7b12f22e9f
KirillYusikov <109421695+Kirill20poshliy@users.noreply.github.com>
2025-09-04 11:57:20 +0300

    chore: add changes

--- src/shared/types/index.d.ts
+++ src/shared/types/index.d.ts
@@ -1,62 +1,62 @@
 interface IUser {
-  id: string;
-  firstname?: string;
-  lastname?: string;
-  name: string;
-  email: string;
-  pict_url?: string | null;
-  isAuth: boolean;
+    id: string;
+    firstname?: string;
+    lastname?: string;
+    name: string;
+    email: string;
+    pict_url?: string;
+    isAuth: boolean;
 }
 
 interface IProject {
-  id: string;
-  name: string;
-  pict_url?: string | null;
-  author_id: string;
-  author_name?: string;
-  created_at: string;
-  updated_at: string;
+    id: string;
+    name: string;
+    pict_url?: string | null;
+    author_id: string;
+    author_name?: string;
+    created_at: string;
+    updated_at: string;
 }
 
 interface IProcess {
-  id: string;
-  name: string;
-  desc: string;
-  project_id: string;
-  project_name: string;
-  content: string;
-  pict_url?: string | null;
-  author_id: string;
-  author_name: string;
-  created_at: string;
-  updated_at: string;
+    id: string;
+    name: string;
+    desc: string;
+    project_id: string;
+    project_name: string;
+    content: string;
+    pict_url?: string | null;
+    author_id: string;
+    author_name: string;
+    created_at: string;
+    updated_at: string;
 }
 
 interface INodeItem {
-  id: string;
-  code: string;
-  name: string;
-  icon?: JSX.Element;
-  description?: string;
-	component?: JSX.Element
-	defaultData: Record<string, unknown>
+    id: string;
+    code: string;
+    name: string;
+    icon?: JSX.Element;
+    description?: string;
+    component?: JSX.Element;
+    defaultData: Record<string, unknown>;
 }
 
 interface IGetAllParams {
-  limit?: number;
-  page?: number;
-  field?: string;
-  order?: string;
-  search?: string;
+    limit?: number;
+    page?: number;
+    field?: string;
+    order?: string;
+    search?: string;
 }
 
 interface IPagination {
-  page?: number,
-  limit?: number,
-  total?: number,
-  total_pages?: number,
-  prev?: string | null,
-  next?: string | null
+    page?: number;
+    limit?: number;
+    total?: number;
+    total_pages?: number;
+    prev?: string | null;
+    next?: string | null;
 }
 
 type NodeCustomData = {
@@ -74,4 +74,9 @@ type NodeCustomData = {
         textDecoration?: string;
         padding?: string;
     };
+};
+
+interface ICredentials {
+    accessToken: string;
+    data: { email: string };
 }
