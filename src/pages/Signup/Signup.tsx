d872abe35cbf2a9b3094a6c304dba11256bb8f9e
Артём Кравцов <a.kravtsov@globexit.ru>
2025-09-17 21:53:36 +0300

    feat: Добавлена тема для страницы регистрации

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@hooks/useTheme";
 import { useRegisterMutation } from "@store/api/user/userApi";
 import type { FormProps } from "antd";
 import { Button, Form, Input, message, Space, Spin } from "antd";
@@ -6,175 +7,176 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 type FieldType = {
-    firstname?: string;
-    lastname?: string;
-    email?: string;
-    password?: string;
-    confirm_password?: string;
+  firstname?: string;
+  lastname?: string;
+  email?: string;
+  password?: string;
+  confirm_password?: string;
 };
 
 export const Signup = () => {
-    const [submittable, setSubmittable] = useState(false);
-    const [form] = useForm();
-    const navigate = useNavigate();
-    const values = Form.useWatch([], form);
+  const { token } = useTheme();
+  const [submittable, setSubmittable] = useState(false);
+  const [form] = useForm();
+  const navigate = useNavigate();
+  const values = Form.useWatch([], form);
 
-    const [register, registerData] = useRegisterMutation();
+  const [register, registerData] = useRegisterMutation();
 
-    const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
-        try {
-            const { firstname, lastname, email, password, confirm_password } =
-                values;
+  const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
+    try {
+      const { firstname, lastname, email, password, confirm_password } = values;
 
-            if (
-                firstname &&
-                lastname &&
-                email &&
-                password &&
-                confirm_password
-            ) {
-                if (password == confirm_password) {
-                    await register({
-                        firstname,
-                        lastname,
-                        email,
-                        password,
-                    }).then((res) => {
-                        if (res.data) {
-                            navigate("/login");
-                        }
-                        if (res.error) {
-                            message.error("Ошибка при регистрации!");
-                        }
-                    });
-                }
+      if (firstname && lastname && email && password && confirm_password) {
+        if (password == confirm_password) {
+          await register({
+            firstname,
+            lastname,
+            email,
+            password,
+          }).then((res) => {
+            if (res.data) {
+              navigate("/login");
             }
-        } catch (e) {
-            message.error("Ошибка сети");
-            console.log(e);
-        } finally {
-            //setLoading(false);
+            if (res.error) {
+              message.error("Ошибка при регистрации!");
+            }
+          });
         }
-    };
+      }
+    } catch (e) {
+      message.error("Ошибка сети");
+      console.log(e);
+    } finally {
+      //setLoading(false);
+    }
+  };
 
-    const onReset = () => {
-        form.resetFields();
-    };
+  const onReset = () => {
+    form.resetFields();
+  };
 
-    useEffect(() => {
-        form.validateFields({ validateOnly: true })
-            .then(() => setSubmittable(true))
-            .catch(() => setSubmittable(false));
-    }, [form, values]);
+  useEffect(() => {
+    form
+      .validateFields({ validateOnly: true })
+      .then(() => setSubmittable(true))
+      .catch(() => setSubmittable(false));
+  }, [form, values]);
 
-    return (
-        <div className="auth-form__container">
-            <div className="auth-form__wrapper">
-                <div className="auth-form__title">
-                    Регистрация в Business Designer
-                </div>
-                <Form
-                    form={form}
-                    name="basic"
-                    layout="vertical"
-                    style={{ width: 450 }}
-                    initialValues={{ remember: true }}
-                    onFinish={onFinish}
-                    autoComplete="off">
-                    <Form.Item<FieldType>
-                        label="Имя"
-                        name="firstname"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Пожалуйста, введите имя!",
-                            },
-                        ]}>
-                        <Input allowClear />
-                    </Form.Item>
-                    <Form.Item<FieldType>
-                        label="Фамилия"
-                        name="lastname"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Пожалуйста, введите фамилию!",
-                            },
-                        ]}>
-                        <Input allowClear />
-                    </Form.Item>
-                    <Form.Item<FieldType>
-                        label="Электронная почта"
-                        name="email"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Пожалуйста, введите почту!",
-                            },
-                        ]}>
-                        <Input allowClear />
-                    </Form.Item>
-                    <Form.Item<FieldType>
-                        label="Пароль"
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Пожалуйста, введите пароль!",
-                            },
-                        ]}>
-                        <Input.Password allowClear />
-                    </Form.Item>
-                    <Form.Item<FieldType>
-                        label="Повторить пароль"
-                        name="confirm_password"
-                        dependencies={["password"]}
-                        rules={[
-                            {
-                                required: true,
-                                message: "Пожалуйста, подтвердите пароль!",
-                            },
-                            ({ getFieldValue }) => ({
-                                validator(_, value) {
-                                    if (
-                                        !value ||
-                                        getFieldValue("password") === value
-                                    ) {
-                                        return Promise.resolve();
-                                    }
-                                    return Promise.reject(
-                                        new Error("Пароли не совпадают!")
-                                    );
-                                },
-                            }),
-                        ]}>
-                        <Input.Password allowClear />
-                    </Form.Item>
-
-                    <Form.Item>
-                        <Space style={{ marginTop: 10 }}>
-                            <Button
-                                type="primary"
-                                htmlType="submit"
-                                disabled={!submittable}>
-                                Зарегистрироваться
-                            </Button>
-                            <Button htmlType="button" onClick={onReset}>
-                                Сбросить
-                            </Button>
-                        </Space>
-                    </Form.Item>
-                    <div className="auth-form__additional">
-                        <span>Есть аккаунт?</span>
-                        <Link to={"/login"}>Войти</Link>
-                    </div>
-                </Form>
-            </div>
-			{
-				registerData.isLoading && (
-					<Spin fullscreen/>
-				)
-			}
+  return (
+    <div
+      className="auth-form__container"
+      style={{ backgroundColor: token.colorBgContainer }}
+    >
+      <div
+        className="auth-form__wrapper"
+        style={{
+          backgroundColor: token.colorBgBase,
+          borderColor: token.colorBorder,
+        }}
+      >
+        <div
+          className="auth-form__title"
+          style={{ color: token.colorTextBase }}
+        >
+          Регистрация в Business Designer
         </div>
-    );
+        <Form
+          form={form}
+          name="basic"
+          layout="vertical"
+          style={{ width: 450 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item<FieldType>
+            label="Имя"
+            name="firstname"
+            rules={[
+              {
+                required: true,
+                message: "Пожалуйста, введите имя!",
+              },
+            ]}
+          >
+            <Input allowClear />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="Фамилия"
+            name="lastname"
+            rules={[
+              {
+                required: true,
+                message: "Пожалуйста, введите фамилию!",
+              },
+            ]}
+          >
+            <Input allowClear />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="Электронная почта"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Пожалуйста, введите почту!",
+              },
+            ]}
+          >
+            <Input allowClear />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="Пароль"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Пожалуйста, введите пароль!",
+              },
+            ]}
+          >
+            <Input.Password allowClear />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="Повторить пароль"
+            name="confirm_password"
+            dependencies={["password"]}
+            rules={[
+              {
+                required: true,
+                message: "Пожалуйста, подтвердите пароль!",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error("Пароли не совпадают!"));
+                },
+              }),
+            ]}
+          >
+            <Input.Password allowClear />
+          </Form.Item>
+
+          <Form.Item>
+            <Space style={{ marginTop: 10 }}>
+              <Button type="primary" htmlType="submit" disabled={!submittable}>
+                Зарегистрироваться
+              </Button>
+              <Button htmlType="button" onClick={onReset}>
+                Сбросить
+              </Button>
+            </Space>
+          </Form.Item>
+          <div className="auth-form__additional">
+            <span>Есть аккаунт?</span>
+            <Link to={"/login"}>Войти</Link>
+          </div>
+        </Form>
+      </div>
+      {registerData.isLoading && <Spin fullscreen />}
+    </div>
+  );
 };
