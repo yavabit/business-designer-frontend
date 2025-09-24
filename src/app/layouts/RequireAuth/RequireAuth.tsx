3cfaf3ff2ab63fb19635d5c97cf67edb0e35513a
Kirill20poshliy <qazasd2014@yandex.ru>
2025-09-24 22:42:37 +0300

    fix: infinite loading

--- src/app/layouts/RequireAuth/RequireAuth.tsx
+++ src/app/layouts/RequireAuth/RequireAuth.tsx
@@ -1,22 +1,13 @@
 import type { RootState } from "@store/index";
-import { type FC, useEffect, useState } from "react";
+import { type FC } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 import { Spin } from "antd";
 
 export const RequireAuth: FC = () => {
     const { isAuth, isLoading, token } = useSelector((state: RootState) => state.user);
-    const [isChecking, setIsChecking] = useState(true);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setIsChecking(false);
-        }, 100);
-
-        return () => clearTimeout(timer);
-    }, []);
-
-    if (isChecking || isLoading) {
+    if ( isLoading) {
         return <Spin size="large" />;
     }
 
