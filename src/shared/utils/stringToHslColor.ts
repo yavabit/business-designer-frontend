4c96747d4a920304499bc5521c0e7077d4b5dc7c
Артём Кравцов <a.kravtsov@globexit.ru>
2025-09-28 17:38:25 +0300

    feat: Изменён тон цвета

--- src/shared/utils/stringToHslColor.ts
+++ src/shared/utils/stringToHslColor.ts
@@ -1,4 +1,4 @@
-export default function stringToHslColor(str: string, s = 30, l = 80) {
+export default function stringToHslColor(str: string, s = 30, l = 60) {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
