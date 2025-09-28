f847ae13b93400a84e1b5e26dbb0e528e2e53136
Артём Кравцов <a.kravtsov@globexit.ru>
2025-09-28 15:56:38 +0300

    feat: Для авторизации по сокету используется access токен

--- src/store/api/socket.ts
+++ src/store/api/socket.ts
@@ -1,9 +1,22 @@
 
 import { io } from "socket.io-client";
 
+
+const userPersist = localStorage.getItem('persist:user')
+let token = ""
+
+if(userPersist) {
+	const user = JSON.parse(userPersist)
+	token = JSON.parse(user.token)
+}
+
+
+
 export const socket = io(import.meta.env.VITE_API_HOST, {
 	transports: ["websocket"],
 	autoConnect: true,
 	withCredentials: true,
+	auth: {
+		token
+	}
 });
-
